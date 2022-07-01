5f791f73c5aaae7726c11d8bc6c337fd07f8f42b
neptune0170 <yg172014@gmail.com>
2022-07-01 18:42:14 +0530

    heap changes

--- STL ALGO/mutating STL algo/makeheap.cpp
+++ STL ALGO/mutating STL algo/makeheap.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main()
 {
      vector<int>v={15,6,7,12,30};
-     make_heap(v.begin(),v.end());
+     make_heap(v.begin(),v.end(),greater<int>());
      cout<<v.front()<<endl;
+     
+     pop_heap(v.begin(),v.end(),greater<int>());
+     cout<<v.front()<<endl;
+     v[4]=2;
+     push_heap(v.begin(),v.end(),greater<int>());
+     cout<<v.front()<<endl;
+     sort_heap(v.begin(),v.end(),greater<int>());
+    
      return 0;
      //make_heap will convert the normal container as a heap// prioroty queue
     
